d2c9401a9bc6ed1e5280db67bd83c12f96951752
w.pan@alumni.ubc.ca <t6o0b@ugrad.cs.ubc.ca>
2018-01-19 21:19:34 -0800

    Implemented a simple shell for echoing the command

--- CSftp.java
+++ CSftp.java
@@ -1,14 +1,38 @@
 import java.io.*;
 import java.net.*;
 
-public class FTPClient {
-    public static void main() throws IOException {
+public class CSftp {
+    public static void main(String[] args) throws IOException {
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("csftp> ");
 
-        String hostName = userInput.readLine();
+        String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
+        try (
+                Socket kkSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            System.out.println("Connection sucessfully established");
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+
+        while(true){
+            System.out.print("csftp> ");
+            String command = userInput.readLine();
+            System.out.println(command);
+
+        }
+
+        /*
         try (
                 Socket kkSocket = new Socket(hostName, portNumber);
                 PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
@@ -39,7 +63,7 @@ public class FTPClient {
                 if (fromUser != null) {
                     System.out.println("Client: " + fromUser);
                     out.println(fromUser);
-                }*/
+                }
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
@@ -49,5 +73,6 @@ public class FTPClient {
                     hostName);
             System.exit(1);
         }
+        */
     }
 }
